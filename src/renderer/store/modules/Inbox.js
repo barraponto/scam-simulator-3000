67a0465f1564ecffbd466d499ed84f64c6c7f60c
Henrique Machado <henriquemg93@gmail.com>
2017-10-21 21:10:31 -0200

    emails

--- src/renderer/store/modules/Inbox.js
+++ src/renderer/store/modules/Inbox.js
@@ -2,17 +2,110 @@ const state = {
   messages: [
     {
       flagged: false,
+      name: 'dreamdad@hotdad.com',
       from: 'Papai',
       subject: 'Aumente seu Tênis',
       date: 'Out 10',
       tag: 'trusted',
+      text: `Desculpa incomodar a sua rotina, mas eu preciso de um favor.
+       Você sabe que eu morro de medo de comprar na internet desde aquela vez que a vó comprou os cogumelos errados pra colocar na sopa.
+       Preciso de ajuda para comprar *TÊNIS NOVOS* para o *SEU IRMÃO*. Ele não para de crescer, o pé dele já chegou no tamanho *42*!
+       E ele tem outra maratona este fim de semana. Maratonas de correr mesmo, não de ficar no computador fazendo videogames...
+      Enfim, eu separei $250 reais para a compra, tudo que vc conseguir de desconto, pode ficar pra você.
+
+      XOXO`,
     },
     {
       flagged: false,
-      from: 'Karai, sua loja de tênis',
+      name: 'Julio Gerente',
+      from: 'JulioGerente@netxooz.com.br',
       subject: 'Promoção de Férias',
       date: 'Out 08',
       tag: 'promo',
+      text: `Você não quer perder a nossa nova promoção!
+      Você está perdendo seus tênis devido ao crescimento rapido da adolescência?
+      Aumente seu tênis HOJE!!!
+      Na troca do seu tênis antigo damos 25% de desconto na sua compra do mesmo modelo em tamanho maior. Isso mesmo 25% de desconto.
+      Confira o regulamento da promoção no nosso site!
+      www.netxooz.com.br/aumenteseutenis
+      Curta nossa página no fakebook.
+
+      Julio Gerente - Gerente de Marketing da Netxooz Enterprises.
+      Netxooz LTDA.`,
+    },
+    {
+      flagged: false,
+      name: 'amaguanda@yuppi.com.hk',
+      from: 'Amaguanda',
+      subject: 'CLIQUE AQUI e receba up two PRODUTOS TOTALMENTE FREE!!!',
+      date: 'Out 10',
+      tag: 'trusted',
+      text: `
+      AQUI ESTÁ A 25% DE COUPON APENAS PARA UM MEMBRO VIP VALORADO!
+      O código do cupom acabou em breve, por favor, conte-nos quais os produtos que você deseja, vamos buscá-los gratuitamente
+      www.shoehero.com/pormo.exe`,
+    },
+    {
+      flagged: false,
+      name: 'SAPATOSBARATOS@SAPATOS.kr',
+      from: 'VENDAS@CAPSNUI.COM.BR',
+      subject: 'FALTA APENAS 1 DIA PARA O FINAL DO SALDÃO DE CALÇADOS!!!!',
+      date: 'Out 09',
+      tag: 'trusted',
+      text: `VOCÊ PRECISA DE UM SAPATO NOVO?
+            SEUS PROBLEMAS ACABARAM! CLIQUE AQUI PARA VER AS PROMOÇÕES MAIS QUENTES NO MUNDO DOS CALÇADOS
+            www.SAPATOSBARATOS.kr
+            É SAPATO PARA TODO LADO, VOCÊ TEM PÉS DE TAMANHOS DIFERENTES? OU SÓ UM PÉ? SEM PROBLEMAS.
+            VENDEMOS PÉS SEPARADAMENTE!!!!
+            VINDOS ESPECIALMENTE DA COREIA DO NORTE! COM OS MELHORES DESIGNERS DE SAPATOS.
+            VOCÊ VAI SE ARREPENDER SE NÃO COMPRAR.
+            OLHE PELA JANELA. ESTÁ VENDO SAPATOS? NÃO? ENTÃO FALTA SAPATOS EM SUA VIDA!
+            COMPRE NOSSOS SAPATOS
+            www.SAPATOSBARATOS.kr
+            É SAPATO PARA DAR E VENDER
+            DE TODOS OS TAMANHOS DE 40 ATÉ 42
+            GOSTOU?
+            KOREANGUY@SAPATOSBARATOS.KR`,
+    },
+    {
+      flagged: false,
+      name: 'kolobus@kolobus.com',
+      from: 'Kolobus',
+      subject: 'Você não vai acreditar no TAMANHO dessa promoção!!',
+      date: 'Set 09',
+      tag: 'trusted',
+      text: `Você merece se dar um presente :D
+      PROMOÇÃO NÃO CUMULATIVA. VÁLIDA SOMENTE PARA PRODUTOS VENDIDOS E ENTREGUES PELA KOLOBUS.
+      EXCETO COMBOS, BICICLETAS, EQUIPAMENTO DE FITNESS, KIT FITNESS, JOGOS, GAMES, ELETRÔNICOS,
+      CATEGORIA DE MONITORAMENTO ESPORTIVO, MONITORES CARDÍACOS, SUPLEMENTOS,
+      GANHE MAIS, MEGA CHANCE, PRE-VENDA, COLLECTION, LANÇAMENTO.
+      As ofertas anunciadas neste e-mail são válidas por 3 dias até as 23:59.
+      Ganhe 20% de desconto em todos os produtos.
+      Em compras acima de 230 reais ganhe um cupon de 100 reais em desconto em sapatos
+      Parcele em até 10x, Pague com Boleto Bancário, Até 30 dias para trocas!
+
+      Este e-mail é um aviso informativo e não significa que você está comprando este produto nem tenha feito uma reserva de compra deste item. As imagens encontradas no e-mail são meramente ilustrativas.
+      O prazo para troca varia de acordo com o tipo de produto, para saber em qual prazo seu produto se aplica acesse nossa página de informações de troca.
+
+      Grupo Kolobus
+      Para esclarecer dúvidas ou enviar sugestões, acesse nossa Central de relacionamento.
+      www.kolobus.com `,
+    },
+    {
+      flagged: false,
+      name: 'cemboi@x90g5.rus.de',
+      from: 'Gerente CEO Cemboi LTDA',
+      subject: ' Deu a louca no gerente! 70% de desconto em toda loja de artigos esportivos!',
+      date: 'Out 10',
+      tag: 'trusted',
+      text: `COMPRA PREMIADA
+        Basta se cadastrar no site, nas compras a partir de R$ 30,00 com seu cartão de crédito na
+        Cemboi, você pode ganhar na hora prêmios instantâneos de R$ 300,00*,
+        além de participar automaticamente do sorteio de 10 superprêmios de R$ 300 mil*
+        no dia 28 de Junho. Participe Agora.
+        Caso seje cliente dos Melhores Bancos você poderá se cadastrar e desfrutar de 60%
+        de desconto em qualquer compra!
+        Clique no botão abaixo e cadastre-se. `,
     },
   ],
 };

8870dfc63b08403636096283e550e95e14352dc1
Capi Etheriel <barraponto@gmail.com>
2017-10-21 18:44:54 -0200

    styled the sidebar

--- src/renderer/components/Inbox.vue
+++ src/renderer/components/Inbox.vue
@@ -1,7 +1,13 @@
 <template>
   <el-container id="email-app">
     <el-aside>
-      <h1>Yuppi Email</h1>
+      <h1 class="logo">Yuppi!</h1>
+      <nav class="labels">
+        <router-link to="/inbox" class="label">Inbox</router-link>
+        <router-link to="/inbox/trusted" class="label">Amigos</router-link>
+        <router-link to="/inbox/promo" class="label">Promoções</router-link>
+        <router-link to="/inbox/misc" class="label">Outros</router-link>
+      </nav>
     </el-aside>
     <el-main class="inbox">
       <div class="search"></div>
@@ -31,5 +37,30 @@
   }
   .el-aside {
     background-color: orange;
+  }
+  .logo {
+    color: white;
+    text-align: center;
+    font-size: 4em;
+  }
+  .labels, .label {
+    display: block;
+  }
+  .label {
+    font-family: Verdana, sans-serif;
+    font-size: 1.2em;
+    line-height: 1.4em;
+    text-decoration: none;
+    text-align: center;
+    background-color: rgba(0,0,0,0.1);
+    color: white;
+    border-radius: 1em;
+    margin: 0.66em 2em;
+  }
+  .label:hover {
+    background-color: rgba(0,0,0,0.3);
+  }
+  .email {
+    display: flex;
   }
 </style>

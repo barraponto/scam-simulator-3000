90cbac4bb7862fe34ec06f80cbd7515b89259137
Capi Etheriel <barraponto@gmail.com>
2017-10-21 20:01:13 -0200

    create Inbox state module

---
+++ src/renderer/store/modules/Inbox.js
@@ -0,0 +1,30 @@
+const state = {
+  messages: [
+    {
+      flagged: false,
+      from: 'Papai',
+      subject: 'Aumente seu Tênis',
+      date: 'Out 10',
+      tag: 'trusted',
+    },
+    {
+      flagged: false,
+      from: 'Karai, sua loja de tênis',
+      subject: 'Promoção de Férias',
+      date: 'Out 08',
+      tag: 'promo',
+    },
+  ],
+};
+
+const mutations = {
+};
+
+const actions = {
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+};

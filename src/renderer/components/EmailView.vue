b063913acfb271490921cf7ee891e1974822bbff
Caio Teixeira <caio.teixeira02@gmail.com>
2017-10-22 02:32:13 -0200

    Estrutura do view

--- src/renderer/components/EmailView.vue
+++ src/renderer/components/EmailView.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>{{message.text}}</p>
+  <div class="mail-container">
+    <div class="mail-header">
+      <div class="mail-subject">{{message.subject}}</div>
+      <div class="mail-sender">De: {{message.name}} ({{message.from}})</div>
+      <div class="mail-date">{{message.date}}</div>
+    </div>
+    <div class="mail-content">{{message.text}}</div>
+  </div>
 </template>
 
 <script>
@@ -16,4 +23,32 @@
 </script>
 
 <style>
+  .mail-container {
+    margin-top: 5px;
+    border: 1px solid #eee;
+    border-radius: 0.33em;
+  }
+  .mail-header {
+    padding: 3%;
+    border-bottom: 1px solid #ccc;
+  }
+  .mail-subject {
+    font: 2.5em small-caps bolder, sans-serif;
+    margin-bottom: 0.5em;
+  }
+  .mail-sender {
+    color: #444;
+    display: inline-block;
+    width: 70%;
+  }
+  .mail-date {
+    color: #444;
+    float: right;
+    text-align: right;
+    display: inline-block;
+    width: 30%;
+  }
+  .mail-content {
+    margin: 3%;
+  }
 </style>

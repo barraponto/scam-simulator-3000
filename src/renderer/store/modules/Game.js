693fe4b7ef3dbd1d23258105e331cbedbe641e45
Capi Etheriel <barraponto@gmail.com>
2017-10-22 06:18:10 -0200

    game state module

---
+++ src/renderer/store/modules/Game.js
@@ -0,0 +1,22 @@
+const state = {
+  level: 1,
+  mission: false,
+};
+
+const mutations = {
+};
+
+const actions = {
+};
+
+const getters = {
+  mission: state => state.mission,
+  level: state => state.level,
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
